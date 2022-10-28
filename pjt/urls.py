50da8ae8ee2ea10b171e55fcbb6fd0c8ecc0f024
mgpo-98 <csi4613@naver.com>
2022-10-28 17:37:09 +0900

    재재배포

--- pjt/urls.py
+++ pjt/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('',include('reviews.urls')),
+    path('',include('reviews.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('reviews/', include('reviews.urls')),
